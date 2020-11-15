b1ca1af6cd636ff04d1a1e578a66e4547b1970df
francisco souza <fsouza@users.noreply.github.com>
2020-11-15 16:21:09 -0500

    Move tl to src

--- tlconfig.lua
+++ tlconfig.lua
@@ -1,8 +1,8 @@
 return {
-  source_dir = 'lua';
+  source_dir = 'src';
   exclude = {'tl_vim.tl'};
   build_dir = 'lua';
-  include_dir = {'lua'};
+  include_dir = {'src'};
   preload_modules = {'tl_vim'};
   skip_compat53 = true;
 }
