6300378ec5b52d0bc5e69e082074a8541d9bc2b9
francisco souza <fsouza@users.noreply.github.com>
2021-01-29 13:51:41 -0500

    bin/save_brew: save url with the tap
    Works well with private taps. Also only use mas if on macos.

--- bin/save_brew.bash
+++ bin/save_brew.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -7,10 +7,18 @@ basedir=$(
 	pwd -P
 )
 
+function get_taps() {
+	for tap in $(brew tap); do
+		user=${tap%%/*}
+		repo=${tap##*/}
+		echo "${tap}" "$(git -C "${HOMEBREW_PREFIX}/Homebrew/Library/Taps/${user}/homebrew-${repo}" remote get-url origin)"
+	done
+}
+
 brew info --installed --json | jq -rf "${basedir}"/extra/brew-info.jq >"$1"
 
-brew tap >"${1}-tap"
+get_taps >"${1}-tap"
 if [[ ${OSTYPE} == darwin* ]]; then
 	brew list --cask >"${1}-cask"
+	mas list >"${1}-mas"
 fi
-mas list >"${1}-mas"
