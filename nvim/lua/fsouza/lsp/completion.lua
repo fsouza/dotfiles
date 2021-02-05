edf1d8e8df761196c3d347c0b0dc32bc867d4a1b
francisco souza <fsouza@users.noreply.github.com>
2021-02-05 00:05:02 -0500

    nvim/lsp/completion: handle popup theme

--- nvim/lua/fsouza/lsp/completion.lua
+++ nvim/lua/fsouza/lsp/completion.lua
@@ -1,3 +1,4 @@
+local api = vim.api
 local M = {}
 
 local function setup(autocomplete, bufnr)
@@ -15,6 +16,14 @@ end
 
 function M.on_attach(bufnr)
   setup(false, bufnr)
+  require('fsouza.color').add_popup_cb(function()
+    local wins = api.nvim_list_wins()
+    for _, winid in ipairs(wins) do
+      if api.nvim_win_is_valid(winid) and pcall(api.nvim_win_get_var, winid, 'compe_documentation') then
+        return winid
+      end
+    end
+  end)
 end
 
 return M
