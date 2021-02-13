3715ca83450b5767b60bd146366b52bbc2052a53
francisco souza <fsouza@users.noreply.github.com>
2021-02-13 02:18:39 -0500

    nvim: simplify code
    lib.fs wasn't being used anywhere else, so embed it.

--- nvim/lua/fsouza/plugin/shortcut.lua
+++ nvim/lua/fsouza/plugin/shortcut.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local vcmd = vim.cmd
+local loop = vim.loop
 
 -- workaround for fzf loading issue. I should just switch to telescope.nvim.
 local _ = vim.fn['fzf#run']
@@ -16,15 +17,18 @@ local function fzf_dir(directory, cd)
 end
 
 function M.register(command, path, cd)
-  local function is_dir_cb(is_dir)
-    if is_dir then
-      fzf_dir(path, cd)
-    else
-      vcmd('edit ' .. path)
-    end
-  end
   M[command] = function()
-    require('fsouza.lib.fs').check_directory(path, vim.schedule_wrap(is_dir_cb))
+    loop.fs_stat(path, function(err, stat)
+      if err then
+        return
+      end
+      local is_dir = stat.type == 'directory'
+      if is_dir then
+        fzf_dir(path, cd)
+      else
+        vcmd('edit ' .. path)
+      end
+    end)
   end
   vcmd(string.format([[command! %s lua require('fsouza.plugin.shortcut')['%s']()]], command,
                      command))
