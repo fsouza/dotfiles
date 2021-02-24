2c238ef9918a9af2fa597825bfa3f40d13e9efcc
francisco souza <fsouza@users.noreply.github.com>
2021-02-23 21:44:21 -0500

    nvim/plugin/mkdir: invoke on startup

--- nvim/lua/fsouza/plugin/mkdir.lua
+++ nvim/lua/fsouza/plugin/mkdir.lua
@@ -10,24 +10,27 @@ local function run(bufnr)
   vfn.mkdir(dir, 'p')
 end
 
-local function register_for_buffer()
-  local bufnr = vfn.expand('<abuf>')
-  helpers.augroup('fsouza__mkdir_' .. bufnr, {
-    {
-      events = {'BufWritePre'};
-      targets = {string.format('<buffer=%d>', bufnr)};
-      modifiers = {'++once'};
-      command = helpers.fn_cmd(function()
-        run(bufnr)
-      end);
-    };
-  })
+local function register_for_buffer(bufnr)
+  bufnr = bufnr or vfn.expand('<abuf>')
+  if api.nvim_buf_get_name(bufnr) ~= '' then
+    helpers.augroup('fsouza__mkdir_' .. bufnr, {
+      {
+        events = {'BufWritePre'};
+        targets = {string.format('<buffer=%d>', bufnr)};
+        modifiers = {'++once'};
+        command = helpers.fn_cmd(function()
+          run(bufnr)
+        end);
+      };
+    })
+  end
 end
 
 function M.setup()
   helpers.augroup('fsouza__mkdir', {
     {events = {'BufNew'}; targets = {'*'}; command = helpers.fn_cmd(register_for_buffer)};
   })
+  register_for_buffer(api.nvim_get_current_buf())
 end
 
 return M
