25497f1518aea1b77e16ce4734597539809ae9fc
francisco souza <fsouza@users.noreply.github.com>
2021-05-13 09:07:40 -0400

    nvim: update efm pre-commit generation to support flake8 from github too

--- nvim/lua/fsouza/lsp/efm.lua
+++ nvim/lua/fsouza/lsp/efm.lua
@@ -213,6 +213,7 @@ local function get_python_tools()
 
   local pc_repo_tools = {
     ['https://gitlab.com/pycqa/flake8'] = get_flake8;
+    ['https://github.com/pycqa/flake8'] = get_flake8;
     ['https://github.com/psf/black'] = get_black;
     ['https://github.com/asottile/add-trailing-comma'] = get_add_trailing_comma;
     ['https://github.com/asottile/reorder_python_imports'] = get_reorder_python_imports;
