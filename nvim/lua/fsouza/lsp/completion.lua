d5329dfa5a4afd779a2522df674d88c167445114
francisco souza <fsouza@users.noreply.github.com>
2021-05-13 19:10:36 -0400

    nvim/lsp/completion: add back my autocompletion hack

--- nvim/lua/fsouza/lsp/completion.lua
+++ nvim/lua/fsouza/lsp/completion.lua
@@ -4,6 +4,15 @@ local helpers = require('fsouza.lib.nvim_helpers')
 
 local M = {}
 
+local function setup(bufnr, autocomplete)
+  require('compe').setup({
+    enabled = true;
+    autocomplete = autocomplete or false;
+    preselect = 'disable';
+    source = {nvim_lsp = true};
+  }, bufnr)
+end
+
 local function cr_key_for_comp_info(comp_info)
   if comp_info.mode == '' then
     return [[<cr>]]
@@ -20,12 +29,7 @@ local cr_cmd = helpers.ifn_map(function()
 end)
 
 function M.on_attach(bufnr)
-  require('compe').setup({
-    enabled = true;
-    autocomplete = false;
-    preselect = 'disable';
-    source = {nvim_lsp = true};
-  }, bufnr)
+  setup(bufnr)
 
   require('fsouza.color').set_popup_cb(function()
     local wins = api.nvim_list_wins()
@@ -36,11 +40,28 @@ function M.on_attach(bufnr)
     end
   end)
 
+  local setup_cmd = helpers.fn_cmd(function()
+    setup(bufnr)
+  end)
+
+  local complete_cmd = helpers.ifn_map(function()
+    setup(bufnr, true)
+    helpers.augroup('fsouza__completion_switch_off', {
+      {
+        events = {'InsertLeave'};
+        targets = {'<buffer>'};
+        modifiers = {'++once'};
+        command = setup_cmd;
+      };
+    })
+    return require('compe')._complete({manual = true})
+  end)
+
   vim.schedule(function()
     helpers.create_mappings({
       i = {
         {lhs = '<cr>'; rhs = cr_cmd; opts = {noremap = true}};
-        {lhs = '<c-x><c-o>'; rhs = [[compe#complete()]]; opts = {expr = true; silent = true}};
+        {lhs = '<c-x><c-o>'; rhs = complete_cmd; opts = {noremap = true}};
         {lhs = '<c-y>'; rhs = [[compe#confirm('<c-y>')]]; opts = {expr = true; silent = true}};
       };
     }, bufnr)
