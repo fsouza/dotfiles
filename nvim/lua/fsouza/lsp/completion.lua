4b0149645feb9bd7470e80368a369050d5614d81
francisco souza <fsouza@users.noreply.github.com>
2021-06-03 09:33:45 -0400

    Revert "nvim/lsp/completion: set min_length"
    This reverts commit 6b6c44123aea0f06211f6367c28a7acd414daa72.

--- nvim/lua/fsouza/lsp/completion.lua
+++ nvim/lua/fsouza/lsp/completion.lua
@@ -8,7 +8,6 @@ local function setup(bufnr, autocomplete)
   require('compe').setup({
     enabled = true;
     autocomplete = autocomplete or false;
-    min_length = 3;
     preselect = 'disable';
     source = {nvim_lsp = true};
   }, bufnr)
