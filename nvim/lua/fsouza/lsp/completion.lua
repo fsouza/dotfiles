a0f215977b4d480d6d2fdcdfaeac092a95db9fcb
francisco souza <fsouza@users.noreply.github.com>
2021-07-19 15:28:54 -0400

    nvim/lsp/completion: fix popup callback

--- nvim/lua/fsouza/lsp/completion.lua
+++ nvim/lua/fsouza/lsp/completion.lua
@@ -32,12 +32,7 @@ function M.on_attach(bufnr)
   setup(bufnr)
 
   require('fsouza.color').set_popup_cb(function()
-    local wins = api.nvim_list_wins()
-    for _, winid in ipairs(wins) do
-      if api.nvim_win_is_valid(winid) and pcall(api.nvim_win_get_var, winid, 'compe_documentation') then
-        return winid
-      end
-    end
+    return require('compe.float').win
   end)
 
   local setup_cmd = helpers.fn_cmd(function()
