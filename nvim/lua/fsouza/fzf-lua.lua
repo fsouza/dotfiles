a957d41e03fba8d282ae106491c0194f96a48735
francisco souza <fsouza@users.noreply.github.com>
2021-08-19 02:34:15 -0400

    nvim/fzf-lua: let fzf pick the border

--- nvim/lua/fsouza/fzf-lua.lua
+++ nvim/lua/fsouza/fzf-lua.lua
@@ -4,13 +4,13 @@ local function fzf_lua()
   if _fzf_lua == nil then
     _fzf_lua = require('fzf-lua')
     _fzf_lua.setup({
-      fzf_args = vim.env.FZF_DEFAULT_OPTS;
+      fzf_args = vim.env.FZF_DEFAULT_OPTS .. ' --border rounded';
       fzf_layout = 'default';
       buffers = {file_icons = false; git_icons = false};
       files = {file_icons = false; git_icons = false};
       grep = {file_icons = false; git_icons = false};
       oldfiles = {file_icons = false; git_icons = false};
-      winopts = {win_height = 0.65; win_width = 0.90};
+      winopts = {win_height = 0.65; win_width = 0.90; win_border = false};
       previewers = {bat = {theme = 'monochrome'}};
     })
   end
