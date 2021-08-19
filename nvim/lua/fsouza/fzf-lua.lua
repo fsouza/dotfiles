1c75dc0873f44c1c7f7cb7bbfa4210ab9f5762be
francisco souza <fsouza@users.noreply.github.com>
2021-08-19 02:57:03 -0400

    nvim: make fzf-lua behave more like fzf.vim with multi-selections

--- nvim/lua/fsouza/fzf-lua.lua
+++ nvim/lua/fsouza/fzf-lua.lua
@@ -1,15 +1,39 @@
 local _fzf_lua = nil
 
+local function edit_or_qf(selected)
+  local actions = require('fzf-lua.actions')
+  if #selected > 2 then
+    actions.file_sel_to_qf(selected)
+    vim.cmd('cc')
+  else
+    actions.file_edit(selected)
+  end
+end
+
+local function file_actions()
+  local actions = require('fzf-lua.actions')
+  return {
+    ['default'] = edit_or_qf;
+    ['ctrl-s'] = actions.file_split;
+    ['ctrl-v'] = actions.file_vsplit;
+    ['ctrl-t'] = actions.file_tabedit;
+    ['ctrl-q'] = actions.file_sel_to_qf;
+  }
+end
+
 local function fzf_lua()
   if _fzf_lua == nil then
+    local actions = file_actions()
+
     _fzf_lua = require('fzf-lua')
     _fzf_lua.setup({
       fzf_args = vim.env.FZF_DEFAULT_OPTS .. ' --border rounded';
       fzf_layout = 'default';
-      buffers = {file_icons = false; git_icons = false};
-      files = {file_icons = false; git_icons = false};
-      grep = {file_icons = false; git_icons = false};
-      oldfiles = {file_icons = false; git_icons = false};
+      buffers = {file_icons = false; git_icons = false; actions = actions};
+      files = {file_icons = false; git_icons = false; actions = actions};
+      git = {files = {file_icons = false; git_icons = false; actions = actions}};
+      grep = {file_icons = false; git_icons = false; actions = actions};
+      oldfiles = {file_icons = false; git_icons = false; actions = actions};
       winopts = {win_height = 0.65; win_width = 0.90; win_border = false};
       previewers = {bat = {theme = 'monochrome'}};
     })
