c0a54b97aa8ab612f42ea0c7df90289119a7bae5
francisco souza <fsouza@users.noreply.github.com>
2021-08-26 23:57:05 -0400

    nvim: query cache_dir and luajit version directly from nvim
    Very hacky, but a non-hacky solution would be too annoying.

--- nvim/scripts/bootstrap.py
+++ nvim/scripts/bootstrap.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
-import argparse
 import asyncio
 import os
 import shutil
 import sys
 from pathlib import Path
+from typing import Literal
+from typing import overload
 
 base_dir = (Path(__file__).parent / "..").absolute()
 
@@ -22,18 +23,47 @@ async def has_command(cmd: str) -> bool:
     return bool(await asyncio.to_thread(shutil.which, cmd))
 
 
+@overload
 async def run_cmd(
     cmd: str | os.PathLike,
     args: list[object],
     cwd: Path | None = None,
     env: dict[str, str] | None = None,
+    capture_output: Literal[False] = False,
 ) -> None:
+    ...
+
+
+@overload
+async def run_cmd(
+    cmd: str | os.PathLike,
+    args: list[object],
+    cwd: Path | None = None,
+    env: dict[str, str] | None = None,
+    capture_output: Literal[True] = True,
+) -> tuple[bytes, bytes]:
+    ...
+
+
+async def run_cmd(
+    cmd: str | os.PathLike,
+    args: list[object],
+    cwd: Path | None = None,
+    env: dict[str, str] | None = None,
+    capture_output: bool = False,
+) -> tuple[bytes, bytes] | None:
+    stdout, stderr = sys.stdout, sys.stderr
+
+    if capture_output:
+        stdout = asyncio.subprocess.PIPE
+        stderr = asyncio.subprocess.PIPE
+
     str_args = [str(arg) for arg in args]
     proc = await asyncio.create_subprocess_exec(
         cmd,
         *str_args,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
+        stdout=stdout,
+        stderr=stderr,
         cwd=cwd,
         env={
             **os.environ,
@@ -41,11 +71,17 @@ async def run_cmd(
         },
     )
 
-    returncode = await proc.wait()
+    stdout, stderr = await proc.communicate()
+    assert proc.returncode is not None
+
+    returncode = proc.returncode
     if returncode != 0:
         raise CommandError(
             f"command '{cmd} {' '.join(str_args)}' exited with status {returncode}",
         )
+
+    if capture_output:
+        return stdout, stderr
 
 
 async def ensure_virtualenv(cache_dir: Path) -> Path:
@@ -83,14 +119,46 @@ async def download_hererocks_py(cache_dir: Path) -> Path:
     return filename
 
 
+async def _neovim_command(cmd: str) -> str:
+    # in an ideal world, we'd start neovim's tcp server and communicate using
+    # the API, but that depends on msgpack, and this script should work with
+    # stuff that's defined in Python's standard library.
+
+    _, stderr = await run_cmd(
+        "nvim",
+        [
+            "--clean",
+            "--headless",
+            "-E",
+            "-u",
+            "NORC",
+            "-R",
+            "-c",
+            cmd,
+            "-c",
+            "qa",
+        ],
+        capture_output=True,
+    )
+
+    return stderr.decode()
+
+
+async def _find_luajit_version() -> str:
+    return await _neovim_command(
+        r"lua v = string.gsub(jit.version, 'LuaJIT ', ''); print(v)",
+    )
+
+
 async def ensure_hererocks(cache_dir: Path) -> Path:
     hr_dir = cache_dir / "hr"
 
     if not await exists(hr_dir):
         hererocks_py = await download_hererocks_py(cache_dir)
+        luajit_version = await _find_luajit_version()
         await run_cmd(
             "python3",
-            [hererocks_py, "-j", "latest", "-r", "latest", hr_dir],
+            [hererocks_py, "-j", luajit_version, "-r", "latest", hr_dir],
         )
 
     await run_cmd(
@@ -199,16 +267,17 @@ async def install_efm(langservers_cache_dir: Path) -> None:
 
 
 async def setup_langservers(cache_dir: Path) -> None:
+    langservers_cache_dir = cache_dir / "langservers"
     await asyncio.gather(
         install_servers_from_npm(),
-        install_ocaml_lsp(cache_dir / "langservers"),
-        install_gopls(cache_dir / "langservers"),
-        install_shfmt(cache_dir / "langservers"),
-        install_efm(cache_dir / "langservers"),
+        install_ocaml_lsp(langservers_cache_dir),
+        install_gopls(langservers_cache_dir),
+        install_shfmt(langservers_cache_dir),
+        install_efm(langservers_cache_dir),
     )
 
 
-async def try_bat_cache_build() -> None:
+async def bat_cache_build() -> None:
     if not await has_command("bat"):
         print("skipping bat")
         return
@@ -216,26 +285,23 @@ async def try_bat_cache_build() -> None:
     await run_cmd("bat", ["cache", "--build"])
 
 
-async def main(cache_dir: Path) -> int:
+async def _find_cache_dir() -> Path:
+    cache_dir = await _neovim_command("echo stdpath('cache')")
+    return Path(cache_dir)
+
+
+async def main() -> int:
+    cache_dir = await _find_cache_dir()
+
     await asyncio.gather(
         setup_langservers(cache_dir),
         ensure_virtualenv(cache_dir),
         ensure_hererocks(cache_dir),
-        try_bat_cache_build(),
+        bat_cache_build(),
     )
 
     return 0
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="bootstrap neovim config")
-    parser.add_argument(
-        "--cache-dir",
-        type=Path,
-        dest="cache_dir",
-        required=True,
-    )
-    args = parser.parse_args()
-    args.cache_dir.mkdir(parents=True, exist_ok=True)
-
-    sys.exit(asyncio.run(main(args.cache_dir)))
+    sys.exit(asyncio.run(main()))
