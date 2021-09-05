e87ff7f060392866573cb76d77579de9dac5238c
francisco souza <fsouza@users.noreply.github.com>
2021-09-05 01:11:08 -0400

    Revert "nvim: make pyright less expensive to setup"
    This reverts commit 79b92b8e136887bfe42b8985d313b3e6e2b98858.

--- nvim/lua/fsouza/lsp/custom/pyright.lua
+++ nvim/lua/fsouza/lsp/custom/pyright.lua
@@ -12,6 +12,30 @@ local function set_from_env_var()
   return nil
 end
 
+local function set_from_poetry()
+  if helpers.filereadable('poetry.lock') then
+    local f = io.popen('poetry env info -p 2>/dev/null', 'r')
+    if f then
+      local virtual_env = f:read()
+      f:close()
+      return virtual_env
+    end
+  end
+  return nil
+end
+
+local function set_from_pipenv()
+  if helpers.filereadable('Pipfile.lock') then
+    local f = io.popen('pipenv --venv')
+    if f then
+      local virtual_env = f:read()
+      f:close()
+      return virtual_env
+    end
+  end
+  return nil
+end
+
 local function set_from_venv_folder()
   local folders = {'venv'; '.venv'}
   for _, folder in pairs(folders) do
@@ -24,7 +48,7 @@ local function set_from_venv_folder()
 end
 
 local function detect_virtual_env(settings)
-  local detectors = {set_from_venv_folder; set_from_env_var}
+  local detectors = {set_from_venv_folder; set_from_env_var; set_from_poetry; set_from_pipenv}
   for _, detect in ipairs(detectors) do
     local virtual_env = detect()
     if virtual_env ~= nil then
