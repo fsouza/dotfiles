23a2344c32792c31687e79d52a5c76425a11884a
francisco souza <fsouza@users.noreply.github.com>
2021-09-07 22:37:52 -0400

    Cleanup some scripts
    Remove some unused stuff and fix permission for some files.

--- bin/save_brew.bash
+++ bin/save_brew.bash
@@ -2,20 +2,7 @@
 
 set -euo pipefail
 
-basedir=$(
-	cd "$(dirname "${0}")"/..
-	pwd -P
-)
-
-function get_taps() {
-	for tap in $(brew tap); do
-		user=${tap%%/*}
-		repo=${tap##*/}
-		echo "${tap}" "$(git -C "${HOMEBREW_PREFIX}/Homebrew/Library/Taps/${user}/homebrew-${repo}" remote get-url origin)"
-	done
-}
-
-brew info --installed --json | jq -rf "${basedir}"/extra/brew-info.jq >"$1"
+brew leaves --installed-on-request >"$1"
 
 get_taps >"${1}-tap"
 if [[ ${OSTYPE} == darwin* ]]; then
