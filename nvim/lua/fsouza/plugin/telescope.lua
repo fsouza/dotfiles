e26e8436878566816099c87b6adc4c03f513833d
francisco souza <fsouza@users.noreply.github.com>
2021-09-12 23:14:40 -0400

    nvim/telescope: override generic sorter with fzf too

--- nvim/lua/fsouza/plugin/telescope.lua
+++ nvim/lua/fsouza/plugin/telescope.lua
@@ -82,7 +82,7 @@ do
     extensions = {
       fzf = {
         fuzzy = true;
-        override_generic_sorter = false;
+        override_generic_sorter = true;
         override_file_sorter = true;
         case_mode = 'smart_case';
       };
