acb35085db45c7312783b453524b5bca99dc5853
francisco souza <fsouza@users.noreply.github.com>
2021-09-21 14:14:01 -0400

    nvim/lsp/diagnostics: use the helper function from vim.diagnostic

--- nvim/lua/fsouza/lsp/diagnostics.lua
+++ nvim/lua/fsouza/lsp/diagnostics.lua
@@ -4,19 +4,6 @@ local api = vim.api
 local lsp = vim.lsp
 local vcmd = vim.cmd
 
-local function items_from_diagnostics(bufnr, diagnostics)
-  local fname = api.nvim_buf_get_name(bufnr)
-  return require('fsouza.tablex').map(function(diagnostic)
-    local pos = diagnostic.range.start
-    return {
-      filename = fname;
-      lnum = pos.line + 1;
-      col = pos.character + 1;
-      text = diagnostic.message;
-    }
-  end, diagnostics)
-end
-
 local function render_diagnostics(items)
   lsp.util.set_qflist(items)
   if vim.tbl_isempty(items) then
@@ -30,22 +17,12 @@ end
 
 function M.list_file_diagnostics()
   local bufnr = api.nvim_get_current_buf()
-  local diagnostics = vim.diagnostic.get(bufnr)
-  if not diagnostics then
-    return
-  end
-
-  local items = items_from_diagnostics(bufnr, diagnostics)
-  render_diagnostics(items)
+  render_diagnostics(vim.diagnostic.toqflist(vim.diagnostic.get(bufnr)))
 end
 
 function M.list_workspace_diagnostics()
-  local all_diagnostics = vim.diagnostic.get(nil)
-  local all_items = require('fsouza.tablex').flat_map(
-                      function(diagnostics, bufnr)
-      return items_from_diagnostics(bufnr, diagnostics)
-    end, all_diagnostics)
-  render_diagnostics(all_items)
+  local diagnostics = vim.diagnostic.get(nil)
+  render_diagnostics(vim.diagnostic.toqflist(diagnostics))
 end
 
 return M
