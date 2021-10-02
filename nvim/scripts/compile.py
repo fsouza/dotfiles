d0d63174de95f728bd769fe04ec2e62bafab1402
francisco souza <fsouza@users.noreply.github.com>
2021-10-02 14:31:18 -0400

    nvim: introduce a helper script for compiling

---
+++ nvim/scripts/compile.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import os
+import pathlib
+import subprocess
+import sys
+from typing import Sequence
+
+
+def main(args: Sequence[str]) -> int:
+    fennel = os.environ["FENNEL"]
+    [fnl_file, out_file] = args
+
+    cmd = subprocess.run([fennel, "-c", fnl_file], capture_output=True)
+    if cmd.returncode != 0:
+        sys.stderr.buffer.write(cmd.stderr)
+        return cmd.returncode
+
+    lua_file = pathlib.Path(out_file)
+    lua_file.parent.mkdir(parents=True, exist_ok=True)
+    lua_file.write_bytes(cmd.stdout)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
