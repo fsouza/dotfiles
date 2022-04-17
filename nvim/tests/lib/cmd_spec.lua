401884478bfb4d6a8e019444e0e612ad1fcf5ede
francisco souza <108725+fsouza@users.noreply.github.com>
2022-04-16 22:45:25 -0400

    nvim: add some tests for lib.cmd

---
+++ nvim/tests/lib/cmd_spec.lua
@@ -0,0 +1,36 @@
+local cmd = require("fsouza.lib.cmd")
+
+local function run_cmd(prg, opts, input_data, timeout)
+  timeout = timeout or 200
+  local result = nil
+
+  cmd.run(prg, opts, input_data, function(r)
+    result = r
+  end)
+  local ok, _ = vim.wait(timeout, function()
+    return result ~= nil
+  end, 25)
+
+  assert(ok)
+  return result
+end
+
+describe("fsouza.lib.cmd", function()
+  it("can run a command and capture stdout & stderr", function()
+    local result = run_cmd(
+      "bash",
+      { args = { "-c", "echo hi from stdout && echo >&2 hi from stderr" } }
+    )
+
+    assert.are.same(0, result["exit-status"])
+    assert.are.same("hi from stdout\n", result.stdout)
+    assert.are.same("hi from stderr\n", result.stderr)
+  end)
+
+  it("can send data to the command", function()
+    local result = run_cmd("cat", { args = {} }, "hello there")
+
+    assert.are.same(0, result["exit-status"])
+    assert.are.same("hello there", result.stdout)
+  end)
+end)
--- nvim/tests/lib/glob_spec.lua
+++ nvim/tests/lib/glob_spec.lua
@@ -37,7 +37,7 @@ describe("compile and match", function()
     assert.is_true(glob.match(matcher, "fule.py"))
     assert.is_not_true(glob.match(matcher, "fzle.py"))
 
-    local ok, matcher = glob.compile("f[a-z]le.py")
+    ok, matcher = glob.compile("f[a-z]le.py")
     assert.is_true(ok)
     assert.is_true(glob.match(matcher, "file.py"))
     assert.is_true(glob.match(matcher, "fble.py"))
@@ -62,54 +62,68 @@ end)
 describe("break", function()
   it("should return a list", function()
     local parts = glob["break"]("**/*.go")
-    assert.are.same({"**/*.go"}, parts)
+    assert.are.same({ "**/*.go" }, parts)
   end)
 
   it("should not break down ranges", function()
     local parts = glob["break"]("**/[abc].go")
-    assert.are.same({"**/[abc].go"}, parts)
+    assert.are.same({ "**/[abc].go" }, parts)
   end)
 
   it("should break down group options", function()
     local parts = glob["break"]("**/*.{go,mod,work}")
-    assert.are.same({"**/*.go"; "**/*.mod"; "**/*.work"}, parts)
+    assert.are.same({ "**/*.go", "**/*.mod", "**/*.work" }, parts)
   end)
 
   it("full paths too", function()
-    local parts = glob["break"]("{/home/user/src/project,/home/user/src/project/pkg1,/home/user/src/project/pkg2,/home/user/src/project/pkg3}")
+    local parts = glob["break"](
+      "{/home/user/src/project,/home/user/src/project/pkg1,/home/user/src/project/pkg2,/home/user/src/project/pkg3}"
+    )
     assert.are.same({
-      "/home/user/src/project";
-      "/home/user/src/project/pkg1";
-      "/home/user/src/project/pkg2";
-      "/home/user/src/project/pkg3";
+      "/home/user/src/project",
+      "/home/user/src/project/pkg1",
+      "/home/user/src/project/pkg2",
+      "/home/user/src/project/pkg3",
     }, parts)
   end)
 
   it("should break down group with special chars", function()
     local parts = glob["break"]("**/*.{go,mod,w?rk}")
-    assert.are.same({"**/*.go"; "**/*.mod"; "**/*.w?rk"}, parts)
+    assert.are.same({ "**/*.go", "**/*.mod", "**/*.w?rk" }, parts)
   end)
 
   it("should break down nested groups", function()
     local parts = glob["break"]("**/*.{go,mod,w{ark,ork}}")
-    assert.are.same({"**/*.go"; "**/*.mod"; "**/*.wark"; "**/*.work"}, parts)
+    assert.are.same({ "**/*.go", "**/*.mod", "**/*.wark", "**/*.work" }, parts)
   end)
 end)
 
 describe("strip", function()
   it("should strip up to the first special char", function()
-    local src = "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload/**"
-    assert.are.same("/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload", glob["strip-special"](src))
+    local src =
+      "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload/**"
+    assert.are.same(
+      "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload",
+      glob["strip-special"](src)
+    )
   end)
 
   it("should return the value unchanged if no special characters are declared", function()
-    local src = "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload/"
-    assert.are.same("/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload/", glob["strip-special"](src))
+    local src =
+      "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload/"
+    assert.are.same(
+      "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-dynload/",
+      glob["strip-special"](src)
+    )
   end)
 
   it("should look back for the path component", function()
-    local src = "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-d?nload/"
-    assert.are.same("/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10", glob["strip-special"](src))
+    local src =
+      "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10/lib-d?nload/"
+    assert.are.same(
+      "/usr/local/Cellar/python@3.10/3.10.2/Frameworks/Python.framework/Versions/3.10/lib/python3.10",
+      glob["strip-special"](src)
+    )
   end)
 
   it("should handle special chars at the beginning", function()
