c5e63fdf3e6ffd998728f3adbca110e0ddfe5842
francisco souza <108725+fsouza@users.noreply.github.com>
2022-11-16 00:37:52 -0500

    nvim/queries: "fix" yaml highlights
    Stop having so many strings everywhere.

---
+++ nvim/queries/yaml/highlights.scm
@@ -0,0 +1,16 @@
+[
+ (double_quote_scalar)
+ (single_quote_scalar)
+] @string
+
+(_
+  value: (flow_node
+           (plain_scalar
+             (string_scalar) @string)))
+
+(block_sequence_item
+  (flow_node
+    (plain_scalar
+      (string_scalar) @string)))
+
+(comment) @comment
