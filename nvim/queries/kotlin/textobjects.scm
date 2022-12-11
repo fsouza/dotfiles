d7a17fd9e4fe8ecea7b961e71badbedda2bc1418
francisco souza <108725+fsouza@users.noreply.github.com>
2022-12-10 21:09:37 -0500

    nvim/queries: add some textobjects for kotlin

---
+++ nvim/queries/kotlin/textobjects.scm
@@ -0,0 +1,9 @@
+(function_declaration) @function.outer
+
+(lambda_literal) @function.outer
+
+(class_declaration) @class.outer
+
+(function_declaration
+  (function_body
+    (statements) @function.inner))
