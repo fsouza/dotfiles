cc3f98a471916352a38630bba4a1b813f0721662
francisco souza <108725+fsouza@users.noreply.github.com>
2024-05-02 22:55:28 -0400

    nvim: remove thrift queries

--- nvim/queries/thrift/locals.scm
+++
@@ -1,7 +0,0 @@
-(struct_definition (identifier) @definition.type)
-
-(enum_definition (identifier) @definition.enum)
-
-(field (identifier) @definition.field)
-
-(const_definition) @definition.const
