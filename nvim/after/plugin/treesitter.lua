2083af2ef850ffc39652dea3ca7713d7c4478e37
francisco souza <108725+fsouza@users.noreply.github.com>
2025-03-02 18:28:35 +0000

    nvim/plugin/treesitter: vendor parsers

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,3 +1,80 @@
+local vendored_parsers = {
+  bash = true,
+  c = true,
+  cmake = true,
+  cpp = true,
+  css = true,
+  cuda = true,
+  diff = true,
+  dockerfile = true,
+  git_config = true,
+  git_rebase = true,
+  gitattributes = true,
+  gitcommit = true,
+  gitignore = true,
+  go = true,
+  gomod = true,
+  gosum = true,
+  gotmpl = true,
+  graphql = true,
+  hcl = true,
+  helm = true,
+  html = true,
+  htmldjango = true,
+  java = true,
+  javascript = true,
+  jinja = true,
+  jinja_inline = true,
+  jq = true,
+  json = true,
+  json5 = true,
+  jsonc = true,
+  jsonnet = true,
+  kotlin = true,
+  lua = true,
+  make = true,
+  markdown = true,
+  markdown_inline = true,
+  nginx = true,
+  nix = true,
+  ocaml = true,
+  ocaml_interface = true,
+  odin = true,
+  perl = true,
+  promql = true,
+  proto = true,
+  python = true,
+  query = true,
+  requirements = true,
+  ruby = true,
+  rust = true,
+  scala = true,
+  sql = true,
+  starlark = true,
+  swift = true,
+  terraform = true,
+  tmux = true,
+  toml = true,
+  tsx = true,
+  typescript = true,
+  vim = true,
+  vimdoc = true,
+  xml = true,
+  yaml = true,
+  zig = true,
+}
+
+local parsers = require("nvim-treesitter.parsers")
+local parser_configs = parsers.get_parser_configs()
+local parser_keys = vim.tbl_keys(parser_configs)
+for _, parser_key in ipairs(parser_keys) do
+  if vendored_parsers[parser_key] == nil then
+    parser_configs[parser_key] = nil
+  else
+    parser_configs[parser_key].install_info.url = vim.fs.joinpath(_G.config_dir, "vendor", "ts-parsers", parser_key)
+  end
+end
+
 local configs = require("nvim-treesitter.configs")
 configs.setup({
   highlight = {
@@ -29,5 +106,4 @@ configs.setup({
   },
   ensure_installed = {},
   auto_install = true,
-  ignore_install = { "phpdoc" },
 })
