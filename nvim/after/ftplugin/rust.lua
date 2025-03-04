acd62378754a75d02687b3af270ef6fce454c990
francisco souza <108725+fsouza@users.noreply.github.com>
2025-03-04 06:31:40 +0000

    nvim/ftplugin/rust: pass `--offline` to cargo

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -3,7 +3,16 @@ servers.start({
   config = {
     name = "rust-analyzer",
     cmd = { vim.fs.joinpath(_G.cache_dir, "langservers", "bin", "rust-analyzer") },
-    settings = { ["rust-analyzer"] = { checkOnSave = { command = "clippy" } } },
+    settings = {
+      ["rust-analyzer"] = {
+        checkOnSave = {
+          command = "clippy",
+        },
+        cargo = {
+          extraArgs = { "--offline" },
+        },
+      },
+    },
     find_root_dir = function()
       return servers.patterns_with_fallback({ "Cargo.toml" })
     end,
